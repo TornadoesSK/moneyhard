f4c99f048f949f5d4d5a1730442fd89a5a10b548
Michal Demko <contact@michaldemko.com>
2024-04-06 22:03:41 +0200

    charts

---
+++ src/components/charts.tsx
@@ -0,0 +1,53 @@
+import { LineChart } from '@mui/x-charts/LineChart';
+
+interface BasicLineChartProps {
+    xAxisData: number[];
+    seriesData: number[];
+    width: number;
+    height: number;
+    xAxisMin?: number;
+    xAxisMax?: number;
+    yAxisMin?: number;
+    yAxisMax?: number;
+  }
+  
+  export default function BasicLineChart({
+    xAxisData,
+    seriesData,
+    width,
+    height,
+    xAxisMin,
+    xAxisMax,
+    yAxisMin,
+    yAxisMax,
+  }: BasicLineChartProps) {
+    return (
+      <LineChart
+        xAxis={[
+          {
+            data: xAxisData,
+            min: xAxisMin,
+            max: xAxisMax,
+            tickNumber: 2,
+            disableLine: true
+          },
+        ]}
+        yAxis={[
+          {
+            min: yAxisMin,
+            max: yAxisMax,
+            tickNumber: 3,
+            disableLine: true
+          },
+        ]}
+        series={[
+          {
+            data: seriesData,
+            showMark: false
+          },
+        ]}
+        width={width}
+        height={height}
+      />
+    );
+  }
