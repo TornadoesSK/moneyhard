2e4416ff5dbf45a926f381ca116befb778346e27
Daniel Forrai <forrai@jednanula.sk>
2024-04-07 10:55:51 +0200

    fix: color

--- src/components/charts/PieChart.tsx
+++ src/components/charts/PieChart.tsx
@@ -3,30 +3,34 @@ import { PieChart } from '@mui/x-charts/PieChart';
 
 export default function BasicPie() {
   return (
-    <PieChart
-      series={[
-        {
-          data: [
-            { id: 0, value: 600, label: 'Free Cash' },
-            { id: 1, value: 300, label: 'Investment' },
-            { id: 2, value: 700, label: 'Fixed expenses' },
-          ],
-          innerRadius: '15%',
-          outerRadius: '80%',
-          paddingAngle: 5,
-          cornerRadius: 5,
-          startAngle: 0,
-          endAngle: 360,
-          cy: '40%',
-        },
-      ]}
-      height={300}
-      slotProps={{
-        legend: {
-          direction: 'row',
-          position: { horizontal: 'middle', vertical: 'bottom' },
-        },
-      }}
-    />
+    <>
+      <style>{'.MuiChartsTooltip-cell {color: #000 !important;}'}</style>
+      <PieChart
+        sx={{ '& .MuiChartsTooltip-cell': { color: 'text.secondary' } }}
+        series={[
+          {
+            data: [
+              { id: 0, value: 600, label: 'Free Cash' },
+              { id: 1, value: 300, label: 'Investment' },
+              { id: 2, value: 700, label: 'Fixed expenses' },
+            ],
+            innerRadius: '15%',
+            outerRadius: '80%',
+            paddingAngle: 5,
+            cornerRadius: 5,
+            startAngle: 0,
+            endAngle: 360,
+            cy: '40%',
+          },
+        ]}
+        height={300}
+        slotProps={{
+          legend: {
+            direction: 'row',
+            position: { horizontal: 'middle', vertical: 'bottom' },
+          },
+        }}
+      />
+    </>
   );
 }
